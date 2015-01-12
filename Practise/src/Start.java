879aea02104a7b8a5f1701ae1c9ebb0584a2fa84
Collin <Collin@Collin-HP>
2015-01-11 21:16:18 -0500

    lol

--- Practise/src/Start.java
+++ Practise/src/Start.java
@@ -1,10 +1,9 @@
-
 public class Start {
 
 	public static void main(String[] args){
 		
 		System.out.println("guise look how impressive this is");
-		im("being serious");
+		im("being super serious");
 	}
 	public static void im(String x){
 		System.out.println(x);
