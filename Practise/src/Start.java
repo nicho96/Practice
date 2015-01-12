7196fd32d540730179a19b85613d78016b4e756b
sspinotti <sarah.4ever.000@gmail.com>
2015-01-11 20:05:26 -0500

    Even. More. Crucial.

--- Practise/src/Start.java
+++ Practise/src/Start.java
@@ -3,5 +3,10 @@ public class Start {
 
 	public static void main(String[] args){
 		
+		System.out.println("guise look how impressive this is");
+		im("being serious");
+	}
+	public static void im(String x){
+		System.out.println(x);
 	}
 }
